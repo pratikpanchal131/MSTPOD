8b96dabe1c3197e71584b428ba91d1695db3fb25
pratikpanchal131 <indianic@ind371.local>
2017-04-06 17:50:39 +0530

    first commit

---
+++ Example/Pods/Target Support Files/MSTPOD/MSTPOD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSTPOD : NSObject
+@end
+@implementation PodsDummy_MSTPOD
+@end
---
+++ Example/Pods/Target Support Files/Pods-MSTPOD_Tests/Pods-MSTPOD_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MSTPOD_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MSTPOD_Tests
+@end
